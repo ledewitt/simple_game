e5d9a19de82a827480784dccbf306e46fc3899ec
Luke DeWitt <ledewitt@gmail.com>
2012-02-15 12:08:32 -0700

    Some more game implementation, look a method

--- simple_game.rb
+++ simple_game.rb
@@ -9,27 +9,31 @@
 
 #### METHODS ####
 
-def describe_location(location map) 
-  
+def describe_location(location) 
+  p location[1]
 end
 
 #### MAIN BODY OF CODE ####
 
 objects = ['WISKEY_BOTTLE', 'BUCKET', 'FROG', 'CHAIN']
 
-LIVING_ROOM = ['You are in the LIVING_ROOM of a wizards house.  There is a wizard snoring loudly on the couch.', 'WEST door garden', 'Upstairs stairway ATTIC']
+LIVING_ROOM = ['LIVING_ROOM', 'You are in the LIVING_ROOM of a wizards house.  There is a wizard snoring loudly on the couch.', 'WEST door garden', 'Upstairs stairway ATTIC']
 
-GARDEN = ['You are in a beautiful GARDEN. There is a WELL in front of you', 'EAST door LIVING_ROOM']
+GARDEN = ['GARDEN', 'You are in a beautiful GARDEN. There is a WELL in front of you', 'EAST door LIVING_ROOM']
 
-ATTIC = ['You are in the ATTIC of the WIZARDS house.  There is a giant welding TORCH in the corner', 'Downstairs stairway LIVING_ROOM']
+ATTIC = ['ATTIC', 'You are in the ATTIC of the WIZARDS house.  There is a giant welding TORCH in the corner', 'Downstairs stairway LIVING_ROOM']
 
 map = [LIVING_ROOM, GARDEN, ATTIC]
 
 object_locations = ['WHISKEY_BOTTLE LIVING_ROOM', 'BUCKET LIVING_ROOM', 'CHAIN GARDEN', 'FROG GARDEN']
 
-location = 'LIVING_ROOM'
+location = map[0]
 
-p objects[0]
+describe_location(location)
 
-p map[0]
-
+# p location[0]
+# 
+# p objects[0]
+# 
+# p map[0]
+# 
