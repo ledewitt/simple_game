8aefc01dc68c1e782004048be11de162a55a8fbc
Luke DeWitt <ledewitt@gmail.com>
2012-02-15 17:03:32 -0700

    Some more game implementation, look a method

--- simple_game.rb
+++ simple_game.rb
@@ -9,17 +9,42 @@
 
 #### METHODS ####
 
-def describe_location(location) 
-  p location[1]
+def look(location)
+  #if location then print description
+  p location[0]
 end
+
+def interpret(command,location)
+  if command == 'look'
+    look(location)
+  end
+end
+
+def start(location)
+  puts "Welcome to a very simple game!"
+  
+  while (command = gets.strip) != 'quit'
+    interpret(command,location)
+  end
+end
+
+# def describe_location(location) 
+#   p location[1]
+# end
+
+# def describe_path(path)
+#   p "There is a #{path[1]} going, #{path[0]} from here"
+# end
 
 #### MAIN BODY OF CODE ####
 
+# Define the games Objects, and Locations
+
 objects = ['WISKEY_BOTTLE', 'BUCKET', 'FROG', 'CHAIN']
 
-LIVING_ROOM = ['LIVING_ROOM', 'You are in the LIVING_ROOM of a wizards house.  There is a wizard snoring loudly on the couch.', 'WEST door garden', 'Upstairs stairway ATTIC']
+LIVING_ROOM = ['You are in the LIVING_ROOM of a wizards house.  There is a wizard snoring loudly on the couch.', 'WEST door garden', 'Upstairs stairway ATTIC', objects[0]]
 
-GARDEN = ['GARDEN', 'You are in a beautiful GARDEN. There is a WELL in front of you', 'EAST door LIVING_ROOM']
+GARDEN = ['You are in a beautiful GARDEN. There is a WELL in front of you', 'EAST door LIVING_ROOM']
 
 ATTIC = ['ATTIC', 'You are in the ATTIC of the WIZARDS house.  There is a giant welding TORCH in the corner', 'Downstairs stairway LIVING_ROOM']
 
@@ -29,7 +54,9 @@ object_locations = ['WHISKEY_BOTTLE LIVING_ROOM', 'BUCKET LIVING_ROOM', 'CHAIN G
 
 location = map[0]
 
-describe_location(location)
+#describe_location(location)
+
+start(location)
 
 # p location[0]
 # 
