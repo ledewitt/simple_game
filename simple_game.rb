5b1f14887453dbcc213375474602b8e4dc1fded6
Luke DeWitt <ledewitt@gmail.com>
2012-02-28 11:28:00 -0700

    Cleaning up thoughts after tutorial

--- simple_game.rb
+++ simple_game.rb
@@ -1,29 +1,28 @@
-### QUESTIONS: ###
+### QUESTIONS: For James ###
 
-### Three shebangs and tab doesn't automatically create  ###
-### a text box for me.                                   ###
-
-
+### Three hashes and tab doesn't automatically create      ###
+### a text box for me.                                     ###
 
 #### MY NOTES - Attempting to level up ####
 
-### Will keep track of what I am doing in comments       ###
-### NOTE: I know this is a poor implementation of coding ###
-###       as this exercise continues I will create a     ###
-###       version using hashes and finally a version     ###
-###       that is fully object oriented in an attempt    ###
-###       to get my feet wet in Ruby programming.        ###
+### Will keep track of what I am doing in comments         ###
+### NOTE: I know this is a poor implementation of coding   ###
+###       just doing a funcitonal program with no objects  ###
+###       as this exercise continues I will create a       ###
+###       version using hashes and finally a version       ###
+###       that is fully object oriented in an attempt      ###
+###       to get my feet wet in Ruby programming.          ###
 
-### Variable soup: Section                               ###
-### Setting up the game enviroment                       ###
+### Variable soup: Section                                 ###
+### Setting up the game enviroment                         ###
 
-### Each room is an array of size three where:           ###
-### 0 index - descriptiong of the location               ###
-### 1 index - array of directions to move                ###
-### 2 index - array of objects in the room               ###
+### Each room is an array of size three where:             ###
+### 0 index - descriptiong of the location                 ###
+### 1 index - array of directions to move                  ###
+### 2 index - array of objects in the room                 ###
 
-### Second itereation of this design will turn these     ###
-### into hashes, third iteration will use objects        ###
+### Second itereation of this design will turn these       ###
+### into hashes, third iteration will use objects          ###
 
 living_room_objects = ['WHISKEY_BOTTLE', 'BUCKET']
 
@@ -53,37 +52,63 @@ location = map[0]
 
 def look(location)
   #if location then print description
-  print location[0], "\n"
-  location[1].each { |x| print "From here there is a #{x}\n" }
+  puts location[0]
+  location[1].each { |x| puts "From here there is a #{x}" }
   if location[2]
-    location[2].each { |x| print "There is a #{x} here.\n" }  
+    location[2].each { |x| puts "There is a #{x} here." }
   end
 end
 
-def # direction(location)
-  
+def walk(location, direction)
+  case location
+  when 'living_room'
+    if direction == 'upstairs'
+      look(map[2])
+      location = map[2]
+    end
+    if direction == 'west'
+      look(map[1])
+      loctioin = map[1]
+    end
+  when 'attic'
+    if direction == 'downstairs'
+      look(map[0])
+      location = map[0]
+    end
+  when 'garden'
+    if direction == 'east'
+      look(map[0])
+      location = map[0]
+    end
+  else
+    puts 'You can not walk that way'
+  end
 end
 
-def # get(location)
-  
-end
+# def # get()
+#   
+# end
 
 def interpret(command,location)
-  if command == 'look'
+  words = command.split(" ")
+  case words.first
+  when ('quit' || 'exit')
+    exit
+  when 'look'
     look(location)
-  end
-  if command == '' # direction
-    
-  end
-  if command == '' # get an object
-    
+  when 'pikcup'
+  
+  when 'walk'
+    walk(location, words.last)
+  else
+    puts 'I do not recognize that command'
   end
 end
 
 def start(location)
   puts "Welcome to a very simple game!"
   
-  while (command = gets.strip) != 'quit'
+  while command = gets.strip
     interpret(command,location)
   end
 end
@@ -99,8 +124,6 @@ end
 #### MAIN BODY OF CODE ####
 
 # Define the games Objects, Locations, and possible directions
-
-
 
 start(location)
 
